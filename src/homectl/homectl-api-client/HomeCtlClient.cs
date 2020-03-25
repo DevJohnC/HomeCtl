44ab9b2eb2cd398d4ec38c6674419063652bb04a
John Carruthers <johnc@frag-labs.com>
2020-03-24 19:03:32 -0500

    Start making homectl-api-client.

---
+++ src/homectl/homectl-api-client/HomeCtlClient.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Net.Client;
+using System.Net.Http;
+
+namespace homectl_api_client
+{
+	public class HomeCtlClient
+	{
+		public HomeCtlClient(HttpClient httpClient)
+		{
+			HttpClient = httpClient;
+		}
+
+		public HttpClient HttpClient { get; }
+
+		private GrpcChannel _grpcChannel;
+		public GrpcChannel GrpcChannel => _grpcChannel ?? CreateGrpcChannel();
+
+		private GrpcChannel CreateGrpcChannel()
+		{
+			_grpcChannel = GrpcChannel.ForAddress(
+				HttpClient.BaseAddress,
+				new GrpcChannelOptions { HttpClient = HttpClient }
+				);
+			return _grpcChannel;
+		}
+	}
+}
