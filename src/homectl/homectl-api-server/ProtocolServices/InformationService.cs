8944b6ffc0fe2a124612c0647b7d9f59ea008e9b
John Carruthers <johnc@frag-labs.com>
2020-04-03 12:36:17 -0500

    Add API for requesting the server's version.

--- src/homectl/homectl-api-server/ProtocolServices/InformationService.cs
+++ src/homectl/homectl-api-server/ProtocolServices/InformationService.cs
@@ -6,13 +6,27 @@ namespace HomeCtl.ApiServer.ProtocolServices
 {
 	class InformationService : Information.InformationBase
 	{
+		private static readonly System.Version AssemblyVersion = typeof(InformationService).Assembly.GetName().Version ?? new System.Version(0, 0);
+
 		public override Task<IpAddressResponse> GetClientIpAddress(Empty request, ServerCallContext context)
 		{
-			return Task.FromResult(
-				new IpAddressResponse
+			return Task.FromResult(new IpAddressResponse
+			{
+				IpAddress = context?.GetHttpContext()?.Connection?.RemoteIpAddress?.ToString() ?? ""
+			});
+		}
+
+		public override Task<ServerVersionResponse> GetServerVersion(Empty request, ServerCallContext context)
+		{
+			return Task.FromResult(new ServerVersionResponse
+			{
+				ApiServerVersion = new Version
 				{
-					IpAddress = context?.GetHttpContext()?.Connection?.RemoteIpAddress?.ToString() ?? ""
-				});
+					Major = AssemblyVersion.Major,
+					Minor = AssemblyVersion.Minor,
+					Name = "automata-indev"
+				}
+			});
 		}
 	}
 }
