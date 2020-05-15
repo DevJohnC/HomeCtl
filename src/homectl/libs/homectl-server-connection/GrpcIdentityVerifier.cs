2a0dc1b478ceaf9bd734e793e685d9db8a15f354
John <johnc@frag-labs.com>
2020-05-14 22:32:38 -0500

    For now only consider a server connected if it responds to a gRPC service call for it's version.

--- src/homectl/libs/homectl-server-connection/GrpcIdentityVerifier.cs
+++ src/homectl/libs/homectl-server-connection/GrpcIdentityVerifier.cs
@@ -1,4 +1,9 @@
-﻿using System.Net.Http;
+﻿using Grpc.Core.Logging;
+using Grpc.Net.Client;
+using HomeCtl.Services.Server;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,12 +11,39 @@ namespace HomeCtl.Connection
 {
 	public class GrpcIdentityVerifier : IServerIdentityVerifier
 	{
-		public Task<bool> VerifyServer(ServerEndpoint serverEndpoint, HttpClient httpClient, CancellationToken stoppingToken)
+		private readonly ILogger<GrpcIdentityVerifier> _logger;
+
+		public GrpcIdentityVerifier(ILogger<GrpcIdentityVerifier> logger)
 		{
+			_logger = logger;
+		}
+
+		public async Task<bool> VerifyServer(ServerEndpoint serverEndpoint, HttpClient httpClient, CancellationToken stoppingToken)
+		{
+			var channel = GrpcChannel.ForAddress(serverEndpoint.Uri, new GrpcChannelOptions
+			{
+				DisposeHttpClient = false,
+				HttpClient = httpClient
+			});
+
+			var client = new Information.InformationClient(channel);
+
+			try
+			{
+				var version = await client.GetServerVersionAsync(Services.Empty.Instance);
+			}
+			catch
+			{
+				_logger.LogDebug($"Server couldn't be reached");
+				return false;
+			}
+
+			_logger.LogDebug($"Contacted server");
+
 			//  todo: query server for identity credentials
-			return Task.FromResult(serverEndpoint.IdentityPolicy.IsValid(
+			return serverEndpoint.IdentityPolicy.IsValid(
 				default
-				));
+				);
 		}
 	}
 }
